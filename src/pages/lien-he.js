ff3c7a896aad64be2ec1d81f6dad2afedf6413c9
phanphuocy <phanphuocy@gmail.com>
2020-02-16 18:33:32 +0700

    scraffolding lien-he page

--- src/pages/lien-he.js
+++ src/pages/lien-he.js
@@ -1,13 +1,68 @@
 import React from "react";
 import { Link } from "gatsby";
+import styles from "../styles/pages/lien-he.module.scss";
 
-import Layout from "../components/reusable/Layout/layout";
+// Import icons
+import { FiMap, FiMail, FiPhone } from "react-icons/fi";
+import { IconContext } from "react-icons";
 
-const IndexPage = () => (
-  <Layout>
-    <h1>This is the About page</h1>
-    <Link to="/">&rarr; back to home</Link>
-  </Layout>
+// Import custom components
+import Header from "../components/reusable/Header/Header";
+import Footer from "../components/reusable/Footer/Footer";
+import WidthConstraint from "../components/reusable/WidthConstraint/WitdhConstraint";
+import MapBox from "../components/specific/lien-he/MapBox/MapBox";
+
+//
+const AddressLine = ({ children, icon }) => (
+  <div className={styles.addressLine}>
+    <IconContext.Provider
+      value={{
+        size: "1.4rem",
+        color: "black",
+        style: { verticalAlign: "middle" }
+      }}
+    >
+      <div>{icon}</div>
+    </IconContext.Provider>
+    <p>{children}</p>
+  </div>
 );
 
-export default IndexPage;
+const ContactPage = () => (
+  <React.Fragment>
+    <Header />
+    <MapBox />
+    <WidthConstraint>
+      <div className={styles.wrapper}>
+        <address className={styles.contacts}>
+          <h1>Lien He</h1>
+          <AddressLine icon={<FiMap />}>
+            <p>126/25/17 Thới An 28, Phường Thới An, Quận 12, TP.HCM</p>
+          </AddressLine>
+          <AddressLine icon={<FiMap />}>
+            <p>
+              72/2/9 Huynh Van Nghe, Ward 15, Tan Binh Dist., Ho Chi Minh City
+            </p>
+          </AddressLine>
+          <AddressLine icon={<FiMail />}>
+            <p>iaicorporation@gmail.com</p>
+          </AddressLine>
+          <AddressLine icon={<FiPhone />}>
+            <p>(028) 2216 4463</p>
+          </AddressLine>
+        </address>
+        <div className={styles.employ}>
+          <h1>Tuyen Dung</h1>
+          <p>
+            Nulla ad sunt est dolor pariatur consequat aliquip do eu.
+            Adipisicing consectetur officia Lorem ea esse ex cillum cillum
+            magna. Minim voluptate quis aliqua esse dolor esse.
+          </p>
+        </div>
+      </div>
+    </WidthConstraint>
+    <Footer />
+  </React.Fragment>
+);
+
+export default ContactPage;
